3604da06d70d5696a84c2cbc5322456bc3786dc2
lini07003 <lini07003@E-PW05C37N>
2024-11-13 12:24:02 +0100

    Add project files.

---
+++ Inlämningsupgift 3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace InlämningsUpgifter_3._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur länge vill du hyra bilen? Svara i timmar");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int kostnadPerTimme = 80;
+            int maxKostnadPerDag = 950;
+
+            int totalKostnad = timmar * kostnadPerTimme;
+
+            if (totalKostnad > maxKostnadPerDag)
+            {
+                totalKostnad = maxKostnadPerDag;
+            }
+
+            Console.WriteLine($"Kostnaden för att hyra bilen i {timmar} timmar är: {totalKostnad} kronor.");
+        }
+    }
+}
